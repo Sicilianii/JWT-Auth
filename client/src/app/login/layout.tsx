b8ccb9859af4d74a9adc4eb923555aa52c701435
Vladislav <joker92006@mail.ru>
2024-06-17 12:38:49 +0300

    cut client

--- client/src/app/login/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Layout({children}: {children: React.ReactNode}) {
-    return (
-        <div className={'bg-[#E0E5EC] w-full h-full absolute transform translate-x-[-50%] translate-y-[-50%] justify-center items-center left-1/2 top-1/2 flex'}>{children}</div>
-    );
-}
-
-export default Layout;
